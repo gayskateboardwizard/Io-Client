52ac70c1e1a404db8c6c9ff55eb8cf50b629deb2
Lev <157486627+gayskateboardwizard@users.noreply.github.com>
2026-02-14 23:04:04 +0100

    Update Macro.java

--- main/java/io/client/modules/Macro.java
+++ main/java/io/client/modules/Macro.java
@@ -2,30 +2,51 @@ package io.client.modules;
 
 import io.client.Category;
 import io.client.Module;
-import io.client.commands.CommandManager;
 import net.minecraft.client.Minecraft;
+import org.lwjgl.glfw.GLFW;
 
 public class Macro extends Module {
     private final String command;
+    private final int[] keyCodes;
 
-    public Macro(String name, String command, int key) {
-        super(name, "Macro: " + command, key, Category.MISC);
+    public Macro(String name, String command, int[] keyCodes) {
+        super(name, "Macro: " + command, keyCodes[keyCodes.length - 1], Category.MISC);
         this.command = command;
+        this.keyCodes = keyCodes;
+    }
+
+    @Override
+    public void toggle() {
+        this.setEnabled(!this.isEnabled());
+        if (this.isEnabled()) {
+            onEnable();
+        } else {
+            onDisable();
+        }
     }
 
     @Override
     public void onEnable() {
         if (Minecraft.getInstance().player != null) {
-            if (command.startsWith("|")) {
-                CommandManager.INSTANCE.handleMessage(command);
-            } else {
-                Minecraft.getInstance().player.connection.sendChat(command);
-            }
+            Minecraft.getInstance().player.connection.sendChat(command);
         }
         setEnabled(false);
     }
 
     public String getCommand() {
         return command;
+    }
+
+    public int[] getKeyCodes() {
+        return keyCodes;
+    }
+
+    public boolean matchesKeys(long window) {
+        for (int keyCode : keyCodes) {
+            if (GLFW.glfwGetKey(window, keyCode) != GLFW.GLFW_PRESS) {
+                return false;
+            }
+        }
+        return true;
     }
 }
