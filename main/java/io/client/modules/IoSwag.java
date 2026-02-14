a7874518629a2a0f196db34c589962a315e2d122
Lev <157486627+gayskateboardwizard@users.noreply.github.com>
2026-02-14 19:34:32 +0100

    Update suffix options in IoSwag class

--- main/java/io/client/modules/IoSwag.java
+++ main/java/io/client/modules/IoSwag.java
@@ -14,12 +14,18 @@ public class IoSwag extends Module {
     public IoSwag() {
         super("IoSwag", "Show Them", -1, Category.MISC);
         suffix.addOption("<IO>");
-        suffix.addOption("木卫一客户端");
+        suffix.addOption("IO on crack!");
+        suffix.addOption("<WK>");
         suffix.addOption("-𝖂𝕶");
+        suffix.addOption("木卫一客户端");
+        suffix.addOption("木卫一");
+        suffix.addOption("イオクライアント");
+        suffix.addOption("イオ");
         suffix.addOption(":3");
         suffix.addOption(":D");
+        suffix.addOption("UwU");
         suffix.addOption("˚⟡˖ ࣪");
-        suffix.addOption("<WK> ");
+        suffix.addOption(">>>");
         addSetting(suffix);
         addSetting(greentext);
 
