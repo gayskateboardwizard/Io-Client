6297bb9bacf3cce59ba5259ab95cdbb5f4ce613d
Lev <157486627+gayskateboardwizard@users.noreply.github.com>
2026-02-14 19:32:02 +0100

    Add macro command and update markchest message

--- main/java/io/client/commands/HelpCommand.java
+++ main/java/io/client/commands/HelpCommand.java
@@ -8,10 +8,12 @@ public class HelpCommand implements Command {
         CommandManager.INSTANCE.sendMessage("§d|cordtarget <x> <y> <z> §7- Sets coordinates target | 'clear' to clear target");
         CommandManager.INSTANCE.sendMessage("§d|toggle <module> §7- Toggles a module");
         CommandManager.INSTANCE.sendMessage("§d|bind <module> <key> §7- Binds a key to a module | 'clear' to unbind");
+        CommandManager.INSTANCE.sendMessage("§d|macro <new|delete|list> §7- Manage macros");
         CommandManager.INSTANCE.sendMessage("§d|friend <username> §7- Adds a player to your friend list");
         CommandManager.INSTANCE.sendMessage("§d|unfriend <username> §7- Removes a player from your friend list");
         CommandManager.INSTANCE.sendMessage("§d|friendlist §7- Shows your friend list");
-        CommandManager.INSTANCE.sendMessage("§d|markchest §7- marks the chest you are looking at for duping");
+        CommandManager.INSTANCE.sendMessage("§d|markchest §7- Marks the chest you are looking at for duping");
+        CommandManager.INSTANCE.sendMessage("§d|bindlist §7- Shows your bind list");
         CommandManager.INSTANCE.sendMessage("§d|help §7- Shows this message");
     }
 }
