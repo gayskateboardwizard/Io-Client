d4e843313a57004f308f9f1d3ec8d4214e31fd1c
FelekDevYT <felsstudio@mail.ru>
2026-02-25 21:34:26 +0300

    IO Client in window name

---
+++ main/java/io/client/mixin/MinecraftClientMixin.java
@@ -0,0 +1,15 @@
+package io.client.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(MinecraftClient.class)
+public class MinecraftClientMixin {
+    @Inject(method = "getWindowTitle", at = @At("RETURN"), cancellable = true)
+    private void onGetWindowTitle(CallbackInfoReturnable<String> cir) {
+        cir.setReturnValue("IO Client");
+    }
+}
